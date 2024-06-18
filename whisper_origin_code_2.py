a9c3085dae36cb062361487858f160ee8e538f36
Nyan <112fkdldjs@naver.com>
2024-06-19 08:26:21 +0900

    실시간 음성인식 테스트. 마지막 단어 인식 안되고, wav파일을 실시간으로 저장하는데서 문제 발생 중

--- whisper_origin_code_2.py
+++ whisper_origin_code_2.py
@@ -5,7 +5,7 @@ model_size = ['tiny', 'base', 'small', 'medium', 'large']
 model = whisper.load_model(model_size[1])
 
 # load audio and pad/trim it to fit 30 seconds
-audio = whisper.load_audio("test2.wav")
+audio = whisper.load_audio("RealTime_STT_with_Whisper/test_audio/test3.wav")
 audio = whisper.pad_or_trim(audio)
 
 # make log-Mel spectrogram and move to the same device as the model
@@ -16,8 +16,11 @@ _, probs = model.detect_language(mel)
 print(f"Detected language: {max(probs, key=probs.get)}")
 
 # decode the audio
-options = whisper.DecodingOptions()
+options = whisper.DecodingOptions(without_timestamps=False)
 result = whisper.decode(model, mel, options)
 
 # print the recognized text
 print(result.text)
+
+print(f'mel shape: {mel.shape}')
+print(f'mel dtype: {mel.dtype}')
